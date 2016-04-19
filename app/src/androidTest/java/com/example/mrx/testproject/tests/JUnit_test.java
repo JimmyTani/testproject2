a6307302f96825aa32246071eca633e90a65bef1
Jimmy <jitani@ucsd.edu>
2016-04-19 13:36:49 -0700

    lemons

--- app/src/androidTest/java/com/example/mrx/testproject/tests/JUnit_test.java
+++ app/src/androidTest/java/com/example/mrx/testproject/tests/JUnit_test.java
@@ -15,6 +15,8 @@ public class JUnit_test extends ActivityInstrumentationTestCase2<MainActivity> {
     }
 
     public void test_first(){
+
+
         mainActivity = getActivity();
         TextView textView = (TextView) mainActivity.findViewById(R.id.tv1);
         String tester = textView.getText().toString();
